aad5f255eef4d77b46e7a6c50e6b9e19bafa047d
boernard <boernard@gmail.com>
2020-03-28 23:27:39 +0100

    don't increment success when aws errors happened

--- bestseller.sh
+++ bestseller.sh
@@ -3,6 +3,13 @@
 #This script expects an input file input.txt containing id,hash,url for an image
 #the images will be stored on S3
 #a log will be created containing the ids of the successfully uploaded images
+#
+#on ubuntu execute this script with 'sudo bash bestseller.sh'
+
+export PATH="/home/ubuntu/bin:/home/ubuntu/.local/bin:/home/ubuntu/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin::$PATH"
+export AWS_ACCESS_KEY_ID=***JAIDQ
+export AWS_SECRET_ACCESS_KEY=***TifHh
+
 
 #set semicolon as delimiter
 IFS=';'
@@ -22,17 +29,21 @@ cat input.txt | while read line; do
   # -s silent mode| -f fail instead of returning http codes | --connect.. max time for connecting | -m max time for whole download
   image=$(curl -s -f --connect-timeout 2 -m 6 ${strarr[2]} | base64 ; exit ${PIPESTATUS[0]})
   if [ $? -eq 0 ]; then
-    echo ${image} | base64 -D | aws s3 cp - s3://fashioncloud/test/${strarr[1]}_original.jpg &&
-    echo ${strarr[0]} >> log.txt
-    # ^^^ append id of successfully written record to log file
-    let "success=success+1"
+    echo ${image} | base64 -d | aws s3 cp - s3://fashioncloud/test/${strarr[1]}_original.jpg
+    if [ $? -eq 0 ]; then
+      echo ${strarr[0]} >> log.txt
+      # ^^^ append id of successfully written record to log file
+      let "success=success+1"
+    else
+      let "errors=errors+1"
+    fi
   else
     let "errors=errors+1"
   fi
   #ouput progress without printing newlines
   printf "\ruploaded: ${success} | errors: ${errors}"
 done
-echo 
+echo
 
 
 #for local testing purposes replace the aws s3 upload line with
